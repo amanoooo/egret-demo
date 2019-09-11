d1440b0436dc768a16975438bd9d330101b06e3d
hikaru amano <amanoooooooooooooooo@gmail.com>
2019-09-11 13:08:06 +0800

    finish profile

--- bin-debug/Profile.js
+++ bin-debug/Profile.js
@@ -8,47 +8,62 @@ var __extends = this && this.__extends || function __extends(t, e) {
 for (var i in e) e.hasOwnProperty(i) && (t[i] = e[i]);
 r.prototype = e.prototype, t.prototype = new r();
 };
-var data = {
+var mockData = {
     life: {
         key: '生命:',
-        value: 0,
+        oldValue: 20,
+        value: 20,
         point: 0,
         minus: 'life-minus',
-        plus: 'life-plus'
+        plus: 'life-plus',
+        scale: 20
     },
     attack: {
         key: '攻击:',
-        value: 0,
+        oldValue: 1,
+        value: 1,
         point: 0,
         minus: 'attack-minus',
-        plus: 'attack-plus'
+        plus: 'attack-plus',
+        scale: 1
     },
     defense: {
         key: '防御:',
-        value: 0,
+        oldValue: 1,
+        value: 1,
         point: 0,
         minus: 'defense-minus',
-        plus: 'defense-plus'
+        plus: 'defense-plus',
+        scale: 1
     },
     speed: {
         key: '敏捷:',
-        value: 0,
+        oldValue: 1,
+        value: 1,
         point: 0,
         minus: 'speed-minus',
-        plus: 'speed-plus'
+        plus: 'speed-plus',
+        scale: 1
     },
 };
-var keys = Object.keys(data);
 var Profile = (function (_super) {
     __extends(Profile, _super);
     function Profile() {
         var _this = _super.call(this) || this;
-        _this.points = 10;
+        _this.info = {
+            points: 10,
+            nick: '尼洛'
+        };
+        _this.data = {};
+        _this.keys = [];
+        _this.data = mockData;
+        _this.keys = Object.keys(mockData);
         return _this;
     }
     Profile.prototype.show = function (stage) {
-        var panel = new eui.Panel();
-        panel['data'] = { points: this.points };
+        this.panel = new eui.Panel();
+        var panel = this.panel;
+        panel['data'] = this.info;
         panel.skinName = "resource/Profile.exml";
         panel.horizontalCenter = 0;
         panel.verticalCenter = 0;
@@ -56,31 +71,50 @@ var Profile = (function (_super) {
         // panel.percentHeight = 50
         console.log('panel', panel);
         var propsGroup = panel.getChildByName('props');
+        var bottomGroup = panel.getChildByName('bottom');
         console.log('propsGroup', propsGroup);
-        for (var i = 0; i < keys.length; i++) {
+        for (var i = 0; i < this.keys.length; i++) {
             var item = new eui.Component();
             item.skinName = 'resource/ProfileItem.exml';
             item.percentWidth = 100;
-            item['data'] = data[keys[i]];
+            item['data'] = this.data[this.keys[i]];
             console.log('item', item);
             propsGroup.addChild(item);
         }
         propsGroup.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onPointClick, this);
+        console.log('bottomGroup', bottomGroup);
+        var cancel = bottomGroup.getChildByName('cancel');
+        console.log('cancel', cancel);
+        var confirm = bottomGroup.getChildByName('confirm');
+        cancel.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onCancelClick, this);
+        confirm.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onConfirmClick, this);
+        // const popup = new eui.TitleWindow()
         stage.addChild(panel);
     };
     Profile.prototype.onPointClick = function (e) {
         var _a = e.target.name.split('-'), name = _a[0], action = _a[1];
+        var _b = this, data = _b.data, keys = _b.keys;
         if (keys.indexOf(name) > -1) {
             console.log(name, action);
-            if (action === 'plus' && this.points > 0) {
+            if (action === 'plus' && this.info.points > 0) {
                 data[name].point++;
-                this.points--;
+                data[name].value = data[name].oldValue + data[name].point * data[name].scale;
+                this.info.points--;
             }
             else if (action === 'minus' && data[name].point > 0) {
                 data[name].point--;
-                this.points++;
+                data[name].value = data[name].oldValue + data[name].point * data[name].scale;
+                this.info.points++;
             }
         }
+    };
+    Profile.prototype.onCancelClick = function (e) {
+        console.log('cancel');
+        this.panel.close();
+    };
+    Profile.prototype.onConfirmClick = function (e) {
+        console.log('confirm');
+        this.panel.close();
     };
     return Profile;
 }(egret.Sprite));
--- src/Profile.ts
+++ src/Profile.ts
@@ -1,46 +1,62 @@
 
-const data = {
+const mockData = {
     life: {
         key: '生命:',
-        value: 0,
+        oldValue: 20,
+        value: 20,
         point: 0,
         minus: 'life-minus',
-        plus: 'life-plus'
+        plus: 'life-plus',
+        scale: 20
     },
     attack: {
         key: '攻击:',
-        value: 0,
+        oldValue: 1,
+        value: 1,
         point: 0,
         minus: 'attack-minus',
-        plus: 'attack-plus'
+        plus: 'attack-plus',
+        scale: 1
 
     },
     defense: {
         key: '防御:',
-        value: 0,
+        oldValue: 1,
+        value: 1,
         point: 0,
         minus: 'defense-minus',
-        plus: 'defense-plus'
+        plus: 'defense-plus',
+        scale: 1
     },
     speed: {
         key: '敏捷:',
-        value: 0,
+        oldValue: 1,
+        value: 1,
         point: 0,
         minus: 'speed-minus',
-        plus: 'speed-plus'
+        plus: 'speed-plus',
+        scale: 1
     },
 }
-const keys = Object.keys(data)
 
 class Profile extends egret.Sprite {
-    points: number = 10
+    info = {
+        points: 10,
+        nick: '尼洛'
+    }
+    data = {}
+    keys = []
+    panel: eui.Panel
     public constructor() {
         super();
+        this.data = mockData
+        this.keys = Object.keys(mockData)
     }
     public show(stage) {
 
-        let panel = new eui.Panel();
-        panel['data'] = { points: this.points }
+        this.panel = new eui.Panel();
+        const { panel } = this
+        panel['data'] = this.info
         panel.skinName = "resource/Profile.exml"
         panel.horizontalCenter = 0;
         panel.verticalCenter = 0;
@@ -52,37 +68,60 @@ class Profile extends egret.Sprite {
 
 
         const propsGroup = panel.getChildByName('props') as eui.Group
+        const bottomGroup = panel.getChildByName('bottom') as eui.Group
 
         console.log('propsGroup', propsGroup);
 
 
-        for (var i = 0; i < keys.length; i++) {
+        for (var i = 0; i < this.keys.length; i++) {
             const item = new eui.Component()
             item.skinName = 'resource/ProfileItem.exml'
             item.percentWidth = 100
-            item['data'] = data[keys[i]]
+            item['data'] = this.data[this.keys[i]]
             console.log('item', item);
 
             propsGroup.addChild(item)
         }
         propsGroup.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onPointClick, this);
 
+        console.log('bottomGroup', bottomGroup);
+
+        const cancel = bottomGroup.getChildByName('cancel')
+        console.log('cancel', cancel);
+
+        const confirm = bottomGroup.getChildByName('confirm')
+        cancel.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onCancelClick, this);
+        confirm.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onConfirmClick, this);
 
 
+
+        // const popup = new eui.TitleWindow()
         stage.addChild(panel);
 
     }
     private onPointClick(e) {
         const [name, action] = e.target.name.split('-')
+        const { data, keys } = this
         if (keys.indexOf(name) > -1) {
             console.log(name, action);
-            if (action === 'plus' && this.points > 0) {
+            if (action === 'plus' && this.info.points > 0) {
                 data[name].point++
-                this.points--
+                data[name].value = data[name].oldValue + data[name].point * data[name].scale
+                this.info.points--
+
             } else if (action === 'minus' && data[name].point > 0) {
                 data[name].point--
-                this.points++
+                data[name].value = data[name].oldValue + data[name].point * data[name].scale
+                this.info.points++
             }
         }
+    }
+    private onCancelClick(e) {
+        console.log('cancel');
+        this.panel.close()
+    }
+    private onConfirmClick(e) {
+        console.log('confirm');
+        this.panel.close()
     }
 }
